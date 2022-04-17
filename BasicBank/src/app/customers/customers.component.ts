2502351177b1c5a4944648e98606af458d9f5145
mohamedabotir <mohamedabotyear@gmail.com>
2022-04-18 01:23:52 +0200

    transfer validation

--- BasicBank/src/app/customers/customers.component.ts
+++ BasicBank/src/app/customers/customers.component.ts
@@ -1,6 +1,9 @@
+import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, Validators } from '@angular/forms';
 import { TransferService } from './../Services/transfer.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../Shared/Users';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-customers',
@@ -8,8 +11,12 @@ import { User } from '../Shared/Users';
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-
-  constructor(private user:TransferService) { }
+  form = this.fb.group({
+    "senderEmail":['',Validators.required],
+    "receiverEmail":['',Validators.required],
+    "money":['',Validators.required]
+  });
+  constructor(private user:TransferService,private fb:FormBuilder,private toast:ToastrService) { }
   customers!:User[];
   ngOnInit(): void {
     this.getCustomers();
@@ -18,5 +25,13 @@ export class CustomersComponent implements OnInit {
 this.user.getAllUsers().subscribe(data=>{
   this.customers = data;
 });
+ }
+ transfer(){
+  if(this.form.status =="VALID"){
+ this.user.postTransfer(this.form.value).subscribe(data=>{
+ });
+}
+ else
+ this.toast.info("Enter Required Fields")
  }
 }
